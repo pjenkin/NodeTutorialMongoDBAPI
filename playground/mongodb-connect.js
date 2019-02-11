dc3a1ec8c8f87782a419c15b0dd8a4d3812ff849
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-11 15:44:58 +0000

    insertOne used to add document to Todo collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser : true }, (error, db) => // error, db object
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser : true }, (error, client) => // error, db object
 {
   if (error)
   {
@@ -8,6 +8,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser : tru
   }
 
   console.log('Connected to MongoDb server');
+  const db = client.db('TodoApp');
 
-  db.close();
+  db.collection('Todos').insertOne(
+    {   // key/value pair and
+      text: 'Another note of some kind',
+      completed: false
+    }, (error, result) =>
+    { // failure
+      if (error)
+      {
+        return console.log('Unable to insert document', error);
+      }
+      // success
+      console.log(JSON.stringify(result.ops, undefined,2));
+    });   // end of insertOne
+
+  client.close();
 });
