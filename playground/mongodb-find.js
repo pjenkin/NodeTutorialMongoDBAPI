69a24f0e55a882b6502351fca703b7dfa9bbb7ce
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-11 18:36:24 +0000

    got documents from collection   .find().toArray().then( ..... 7-65

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // ES6 object destructuring
+
+// var objID = new ObjectID();
+// console.log(objID);
+// return;   // temporary, for flow
+// var user = {name: 'P N Jenkin', age:45};
+// // ES6 object destructuring
+// var {name} = user;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser : true }, (error, client) => // error, db object
+{
+  if (error)
+  {
+    return console.log(`Unable to connect to MongoDb ${error}`);    // return only for flow purposes
+  }
+
+  console.log('Connected to MongoDb server');
+  const db = client.db('TodoApp');
+
+  db.collection('Todos').find().toArray().then((documents) =>
+  {
+    // then-resolve?
+    console.log('Todos (see below)');
+    console.log(JSON.stringify(documents, undefined, 2));
+    // print documents
+  },
+  (error) =>   // then-reject?
+  {
+      console.log('Unable to fetch Todos documents',error);
+  });
+  // find with no arguments - all documents - returns cursor
+  // use a promise (toArray) with then
+
+  client.close();
+});
