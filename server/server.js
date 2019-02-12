d7d2226b3736670f7b0a1b202016e63825bb2cd2
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-12 15:08:30 +0000

    model (~schema) for mongoose for Todo documents/records and create 1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 // mongoose configured
+var Todo = mongoose.model('Todo', {
+  text:
+  {
+    type: String
+  },
+  completed:
+  {
+    type: Boolean
+  },
+  completedAt:
+  {
+    type: Number    // standard UNIX timestamp
+  }
+});
+// model cf schema https://mongoosejs.com/docs/guide.html#models
+
+var newTodo = new Todo({text: 'mash potatoes'});
+
+newTodo.save().then((document) =>
+{
+  console.log('Saved document/record: ', document);
+}, (error) =>
+{
+  console.log('Unable to save document/record', error);
+});
