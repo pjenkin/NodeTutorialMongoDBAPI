f422b68c7ba62edea7503ffd493a36faa224a6a0
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-12 10:05:25 +0000

    deleteMany to find & delete 7-67 (huge result printout with result: { n: 3, ok: 1 } for 3 deletions)

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,69 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // ES6 object destructuring
+
+// var objID = new ObjectID();
+// console.log(objID);
+// return;   // temporary, for flow
+// var user = {name: 'P N Jenkin', age:45};
+// // ES6 object destructuring
+// var {name} = user;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser : true }, (error, client) => // error, db object
+{
+  if (error)
+  {
+    return console.log(`Unable to connect to MongoDb ${error}`);    // return only for flow purposes
+  }
+
+  console.log('Connected to MongoDb server');
+  const db = client.db('TodoApp');
+
+
+  // deleteMany
+  db.collection('Todos').deleteMany({text:'eat crowst'}).then((result) =>
+  { // only cover sucess/fulfilment for the moment
+      console.log(result);
+  });
+
+  // deleteOne
+
+
+  //findOneAndDelete
+
+
+  // db.collection('Todos').insertOne(
+  //   {   // key/value pair and
+  //     text: 'Another note of some kind',
+  //     completed: false
+  //   }, (error, result) =>
+  //   { // failure
+  //     if (error)
+  //     {
+  //       return console.log('Unable to insert document', error);
+  //     }
+  //     // success
+  //     console.log(JSON.stringify(result.ops, undefined,2));
+  //   });   // end of insertOne
+
+
+  // challenge - insert new doc into Users collection (properties - name, age, location)
+  // db.collection('Users').insertOne(
+  //   {
+  //     name: 'P N Jenkin',
+  //     age: 45,
+  //     location: 'Redruth, Kernow'
+  //   }, (error, result) =>
+  //   {
+  //     // failure
+  //     if (error)
+  //     {
+  //       return console.log('Unable to insert document to Users', error)
+  //     }
+  //     //success
+  //     // console.log(JSON.stringify(result.ops,undefined,2));
+  //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(),undefined,2));
+  //   });  // end of insertOne for Users
+
+
+  // client.close();
+});
