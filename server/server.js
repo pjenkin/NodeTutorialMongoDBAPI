1409c8a4c5555d610cbc274b376b3c6caf98773d
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-12 16:42:29 +0000

    define 'User' model and add document/record to Users - challenge 7-71

--- server/server.js
+++ server/server.js
@@ -8,18 +8,46 @@ var Todo = mongoose.model('Todo', {
   {
     type: String,
     required: true,
-    minlength: 1
+    minlength: 1,
+    trim: true
   },
   completed:
   {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt:
   {
-    type: Number    // standard UNIX timestamp
+    type: Number,    // standard UNIX timestamp
+    default: null
   }
 });
 // model cf schema https://mongoosejs.com/docs/guide.html#models
+
+// challenge 7-71 User email property require & trim & string minlength 1
+
+var User = mongoose.model('User',
+{
+  email:
+  {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+}
+);
+
+// var stinkyUser = new User({email: ''}).save().then((document) =>   // deliberately wrong
+var stinkyUser = new User({email: 'stinky@example.com'}).save().then((document) =>   // deliberately wrong
+{
+  console.log('Added Stinky as an user', document);
+},
+(error) =>
+{
+  console.log('Alas, error adding Stinky as an user: ', error.message);
+}
+);
 
 // var newTodo = new Todo({text: 'mash potatoes'});
 //
@@ -32,13 +60,13 @@ var Todo = mongoose.model('Todo', {
 // });
 
 // challenge 7-70 - Create record with model's property values completed
-var serveTodo = new Todo({text: 'serve mashed potatoes', completed: false, completedAt: null});
-
-serveTodo.save().then((document) =>
-{
-  console.log('Saving \'serve mashed potatoes\'...');
-  console.log('Saved document/record: ', document);
-}, (error) =>
-{
-  console.log('Unable to save document/record: ', error);
-});
+// var serveTodo = new Todo({text: 'serve mashed potatoes', completed: false, completedAt: null});
+//
+// serveTodo.save().then((document) =>
+// {
+//   console.log('Saving \'serve mashed potatoes\'...');
+//   console.log('Saved document/record: ', document);
+// }, (error) =>
+// {
+//   console.log('Unable to save document/record: ', error);
+// });
