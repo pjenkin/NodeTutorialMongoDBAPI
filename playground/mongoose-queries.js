268f30b887c943d5d9e364a3f634b07fbfa00fb4
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-13 18:25:08 +0000

    check for missing id & 'slightly' validate id using mongodb.ObjectID.isValid

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,27 +1,39 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
+const {ObjectID} = require('mongodb');
+
 var id = '5c6444bf027af267b07ca0ac';
+// var id = '5c6444bf027af267b07ca0ac11';  // invalid - too long
 
-Todo.find(
-  {
-    _id: id         // NB mongoose doesn't require ObjectID object for ID
-  })
-  .then((todos) =>
+if (!ObjectID.isValid(id))
 {
-  console.log('Todos', todos);
-});
+  console.log('ID not valid');
+}
 
-Todo.findOne(      // cf firstOrDefault??
-{
-  _id: id           // NB mongoose doesn't require ObjectID object for ID
-})
-.then((todo) =>     // just 1 todo
-{
-  console.log('Todos', todo);
-});
+// Todo.find(
+//   {
+//     _id: id         // NB mongoose doesn't require ObjectID object for ID
+//   })
+//   .then((todos) =>
+// {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne(      // cf firstOrDefault??``
+// {
+//   _id: id           // NB mongoose doesn't require ObjectID object for ID
+// })
+// .then((todo) =>     // just 1 todo
+// {
+//   console.log('Todos', todo);
+// });
 
 Todo.findById(id).then((todo) =>     // by ID - just 1 todo
 {
+  if (!todo)
+  {
+    return console.log('ID not found');
+  }
   console.log('Todos', todo);
-});
+}).catch((error) => console.log(error));
