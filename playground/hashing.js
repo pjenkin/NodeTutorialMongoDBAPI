830d0940f636a308beb95c0007d32ab8d8350b96
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-15 18:45:03 +0000

    hash un-salted

--- playground/hashing.js
+++ playground/hashing.js
@@ -5,3 +5,13 @@ var hash = SHA256(message).toString();
 
 console.log(`Message ${message}`);
 console.log(`Hash ${hash}`);    // useful for comparison of entered/stored password
+
+var data = {
+  id: 4     // data to send to client
+};
+
+var token =
+{
+  data,   // ES6 abbreviation
+  hash: SHA256(JSON.stringify(data)).toString   // for checking
+}
