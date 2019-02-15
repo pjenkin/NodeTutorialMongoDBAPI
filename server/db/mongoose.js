b581b8b1778764cf3c7a8a9307b00f1ddcaf31c5
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-15 11:36:03 +0000

    test & development environments branching for separate db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 // mongoose configured
 module.exports = {
   mongoose    // mongoose: mongoose ES6
--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
+var env = process.env.NODE_ENV || 'development'    // heroku only?
+console.log('env *****', env);
+
+if (env === 'development')
+{
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}
+else if (env === 'test')
+{
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');  // E6 destructuring
