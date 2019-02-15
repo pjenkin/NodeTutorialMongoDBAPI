0cd7df74baba8a99edc360070284eb41e22bbc13
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-15 14:18:07 +0000

    user model amended to include email validation (npm i'ing validator) & basic password & token info, 8-88.

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');   // const not really better than var for require
+const validator = require('validator');
 
 // challenge 7-71 User email property require & trim & string minlength 1
 
@@ -9,8 +10,37 @@ var User = mongoose.model('User',
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {     /* https://mongoosejs.com/docs/validation.html#custom-validators */
+      /*validator: (value) =>
+      {
+        return validator.isEmail(value);
+        // simplified below
+      },*/
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password:     /* plain text password at this stage */
+  {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens:       /* tokens as array not universally available out-of-the-box  */
+  [
+    {
+      access: {
+        type: String,
+        required: true
+       },
+      token: {
+      type: String,
+      required: true
+     }
+    }
+  ]
 }
 );
 
