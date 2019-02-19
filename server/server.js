393fb6c5c917fd23ee3a63e1230ca9a9aff269a1
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-19 12:50:30 +0000

    ... refactoring JWT authenticate into separate file 8-91

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {mongoose} = require('./db/mongoose');  // E6 destructuring
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -213,33 +214,6 @@ app.post('/users',(request, response) =>    // POST for todos
 });   // end of /users POST route
 
 
-// bespoke middleware authentication (3 arguments - request, response, next : response() only run when next() called)
-var authenticate = (request, response, next) =>
-{
-  var token = request.header('x-auth');       // get specific HTTP header to fetch
-
-  User.findByToken(token).then((user) =>
-  {
-    if (!user)
-    {
-      // return Promise.reject();              // enclosing code, as written, will return status 401
-      return Promise.reject('Error: User not found').catch(error => {console.log('caught user not found Promise reject error');console.log(error); if (error === 'Error: User not found') {response.status(401).send();}});
-      // could have returned reject value; would have been error caught in enclosing code -
-      // NB need to catch rejection error
-      // enclosing code, as written, would have returned status 401 ...
-      // ... however, had to (1) catch rejection error and call response here, ...
-      // ... and (2) (deprecation) use response.sendStatus(401) instead of just response.send(401)
-    }
-    // response.send(user);
-    request.user = user;    // add request properties for use in response
-    request.token = token;
-    next();
-  })
-  .catch((error) =>
-{
-  response.status(401).send();
-});
-};
 
 // initial private route
 app.get('/users/me', authenticate, (request, response) =>
